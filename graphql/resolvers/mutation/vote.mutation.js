78e4cfc16f2e3a4d1cd99fa102550de3e1a9b1ec
mrtkprc <123456ORKebru>
2020-04-05 22:05:09 +0300

    isVoteGiven is added during inserting Vote.

--- graphql/resolvers/mutation/vote.mutation.js
+++ graphql/resolvers/mutation/vote.mutation.js
@@ -1,6 +1,6 @@
 module.exports = {
     addVote: async (parent, {data: {vote, participantId, sessionId}}, {Vote, pubSub}) => {
-        const voteGiven = await Vote.findOneAndUpdate({participantId, sessionId},{$set:{vote}},{upsert: true, new: true});
+        const voteGiven = await Vote.findOneAndUpdate({participantId, sessionId},{$set:{vote, isGiven: 1}},{upsert: true, new: true});
 
         await pubSub.publish('voteGiven',{
             voteGiven
