ebde62bbff377010d538c11bd1cefa996db9cc0f
mrtkprc <123456ORKebru>
2020-04-05 18:56:26 +0300

    Deprecation warning is fixed.

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 module.exports = () => {
     mongoose
-        .connect(process.env.DB_URI, { useCreateIndex: true,useNewUrlParser: true, useUnifiedTopology: true })
+        .connect(process.env.DB_URI, { useCreateIndex: true,useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
         .then(() => console.log('Connected to MongoDB'))
         .catch(e => console.log(e));
 };
